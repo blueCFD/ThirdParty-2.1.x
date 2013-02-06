261c70b3516cfa79a3b3e9e17570d5465e9cea0e
Bruno Santos <bruno.santos@bluecape.com.pt>
2013-02-06 15:15:13 +0000

    Shifted back the gcc version to 4.5 series, but upped from 4.5.3 to 4.5.4.

--- etc/defaults.sh
+++ etc/defaults.sh
@@ -61,17 +61,17 @@ mingw32 | mingw-w32 | mingw-w64 | i686-w64-mingw32 | x86_64-w64-mingw32)
 #     mpfr_version="mpfr-2.4.2"
 #     binutils_version="binutils-2.21"
 
-#     gcc_version="gcc-4.5.3"
-#     gmp_version="gmp-4.3.2"
-#     mpfr_version="mpfr-2.4.2"
-#     mpc_version="mpc-0.8.2"
-#     binutils_version="binutils-2.21"
+    gcc_version="gcc-4.5.4"
+    gmp_version="gmp-4.3.2"
+    mpfr_version="mpfr-2.4.2"
+    mpc_version="mpc-0.8.2"
+    binutils_version="binutils-2.21"
 
-   gcc_version=gcc-4.6.3
-   gmp_version=gmp-5.0.2
-   mpfr_version=mpfr-3.0.1
-   mpc_version=mpc-0.9
-   binutils_version="binutils-2.22.52"
+#     gcc_version=gcc-4.6.3
+#     gmp_version=gmp-5.0.2
+#     mpfr_version=mpfr-3.0.1
+#     mpc_version=mpc-0.9
+#     binutils_version="binutils-2.22.52"
 
     #MinGW versions
     mingw32rt_version="mingwrt-3.20"
